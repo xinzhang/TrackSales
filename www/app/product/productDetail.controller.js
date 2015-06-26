5e83d8058185919ad51a050f6d4f49210f2df309
xin.zhang <xin_zhang_2000@yahoo.com>
2015-06-26 17:05:36 +1000

    add the pages

---
+++ www/app/product/productDetail.controller.js
@@ -0,0 +1,16 @@
+﻿angular.module('app')
+
+.controller('ProductDetailCtrl', function ($scope, $ionicPopup, $ionicViewService, $state, $stateParams, localStorageService, $firebaseArray) {
+    $scope.title = "Product";
+
+    active();
+
+    function active() {
+    }
+
+    $scope.save = function () {
+
+        //ClientService.save($scope.client);
+        $state.go('app.products');
+    }
+});
